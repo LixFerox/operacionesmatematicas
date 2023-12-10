e4da2457ed6fd318fb3e6a28eed774487e8e0685
LixFerox <144841907+LixFerox@users.noreply.github.com>
2023-12-10 13:09:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let number = 0;
+function add(num) {
+  number++;
+  document.getElementById("number").value = number;
+}
+function sub(num) {
+  number--;
+  document.getElementById("number").value = number;
+}
